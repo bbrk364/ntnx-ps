98f13337bf45204bbbcae43a4e6f6f7718651df6
cloudcor-ntnx <55808532+cloudcor-ntnx@users.noreply.github.com>
2019-09-25 12:56:17 -0700

    Update get-ntnxvm.ps1

--- get-ntnxvm.ps1
+++ get-ntnxvm.ps1
@@ -1,2 +1,2 @@
-$vms = get-ntnxvm |select vmName, uuid, hostName, nonNdfsDetails, powerState
+$vms = get-ntnxvm |select ipaddresses, vmName, uuid, hostName, nonNdfsDetails, powerState |ft -autosize
 $vms
