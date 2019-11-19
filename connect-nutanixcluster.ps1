f69f8d64f4128bdcf0d5bff67cb310435747bc34
cloudcor-ntnx <55808532+cloudcor-ntnx@users.noreply.github.com>
2019-11-19 12:18:20 -0800

    Update connect-nutanixcluster.ps1

--- connect-nutanixcluster.ps1
+++ connect-nutanixcluster.ps1
@@ -1,5 +1,15 @@
 $myvarLoaded = Get-PSSnapin -Name NutanixCmdletsPSSnapin -ErrorAction SilentlyContinue | % {$_.Name}
 if ($myvarLoaded -eq $null){Add-PSSnapin NutanixCmdletsPSSnapin}
-$ntnxcluster = READ-host "Name/IP of cluster?"
-connect-nutanixcluster -name $ntnxcluster -AcceptInvalidSSLCerts
+# Cluster IP
+$server = 'Your NTNX CLUSTER IP'
+
+# Request & Verify credentials 
+$credentials = Get-Credential -Message "**Enter your Nutanix Admin credentials here**"
+
+$username = $credentials.username
+$password = ConvertTo-SecureString $($credentials.GetNetworkCredential().password) -AsPlainText -Force
+
+write-host "Connecting to Cluster..."
+Connect-NTNXCluster -Server $server -UserName $UserName -Password $password -AcceptInvalidSSLCerts -ForcedConnection
+write-host "Done!"
 invoke-expression $($connect.content)
