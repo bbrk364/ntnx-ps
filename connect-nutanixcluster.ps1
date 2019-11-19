8423368b95ed63b8dd0c292a123b8ce6a3f275aa
cloudcor-ntnx <55808532+cloudcor-ntnx@users.noreply.github.com>
2019-11-19 12:58:25 -0800

    Update connect-nutanixcluster.ps1

--- connect-nutanixcluster.ps1
+++ connect-nutanixcluster.ps1
@@ -5,5 +5,5 @@ $server = 'Your NTNX CLUSTER IP'
 write-host "Connecting to Cluster..."
 Connect-NTNXCluster -Server $server -AcceptInvalidSSLCerts -ForcedConnection
 write-host "Done!"
-invoke-expression $($connect.content)
+
 
