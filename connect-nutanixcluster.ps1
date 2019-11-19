c723dcca8f3aef9a8bff9a9d9f2ba3d1122e2533
cloudcor-ntnx <55808532+cloudcor-ntnx@users.noreply.github.com>
2019-11-19 12:01:16 -0800

    Update connect-nutanixcluster.ps1

--- connect-nutanixcluster.ps1
+++ connect-nutanixcluster.ps1
@@ -1,2 +1,4 @@
+$myvarLoaded = Get-PSSnapin -Name NutanixCmdletsPSSnapin -ErrorAction SilentlyContinue | % {$_.Name}
+if ($myvarLoaded -eq $null){Add-PSSnapin NutanixCmdletsPSSnapin}
 connect-nutanixcluster -AcceptInvalidSSLCerts
 invoke-expression $($connect.content)
