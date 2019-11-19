1ebe8b5bc0e9462d9018265614b2a5b57d405b6d
cloudcor-ntnx <55808532+cloudcor-ntnx@users.noreply.github.com>
2019-11-19 12:03:53 -0800

    Update connect-nutanixcluster.ps1

--- connect-nutanixcluster.ps1
+++ connect-nutanixcluster.ps1
@@ -1,4 +1,5 @@
 $myvarLoaded = Get-PSSnapin -Name NutanixCmdletsPSSnapin -ErrorAction SilentlyContinue | % {$_.Name}
 if ($myvarLoaded -eq $null){Add-PSSnapin NutanixCmdletsPSSnapin}
-connect-nutanixcluster -AcceptInvalidSSLCerts
+$ntnxcluster = READ-host "Name/IP of cluster?"
+connect-nutanixcluster -name $ntnxcluster -AcceptInvalidSSLCerts
 invoke-expression $($connect.content)
