9c6618252970f968e7318d3a703966132d81b99e
cloudcor-ntnx <55808532+cloudcor-ntnx@users.noreply.github.com>
2019-11-25 15:17:40 -0800

    Update get-container-details.ps1

--- get-container-details.ps1
+++ get-container-details.ps1
@@ -1,2 +1,2 @@
-$container = Get-NTNXContainer | select @{name="Container_ID";expression={$_.id.substring($_.id.indexof(":") + 2)}},Name,@{name="Free_Space_[gb]";expression={((Get-NTNXContainerStat -id ($_.id.substring($_.id.indexof(":") + 2)) -Metrics storage.user_free_bytes).values)}}
+$container = Get-NTNXContainer | select @{name="Container_ID";expression={$_.id.substring($_.id.indexof(":") + 2)}},Name,@{name="Free_Space_[kb]";expression={((Get-NTNXContainerStat -id ($_.id.substring($_.id.indexof(":") + 2)) -Metrics storage.user_free_bytes).values)}}
 $container
