0b7f6e0ec3be6359d2d0526e2cc2ec7b5583ba36
cloudcor-ntnx <55808532+cloudcor-ntnx@users.noreply.github.com>
2019-11-26 11:58:21 -0800

    Update whatitdo.ps1

--- whatitdo.ps1
+++ whatitdo.ps1
@@ -5,6 +5,7 @@ function Show-Menu
     )
     Clear-Host
     Write-Host "================ $Title ================"
+    Write-Host "X: Press 'X' to connect to a Nutanix Cluster
     Write-Host "1: Press '1' to list all the Vms."
     Write-Host "2: Press '2' to list all the hosts."
     Write-Host "3: Press '3' to list containers"
@@ -12,6 +13,13 @@ function Show-Menu
     Write-Host "5: Press '5' to do next thing "
     Write-Host "6: Press '6' to do next thing "
     Write-Host "Q: Press 'Q' to quit."
+}
+
+function List-VMs
+{
+Write-Host "Script Block to Display all the VMs"
+$CONNECT = invoke-webrequest https://raw.githubusercontent.com/cloudcor-ntnx/ntnx-ps/master/connect-nutanixcluster.ps1
+invoke-expression $($connect.content)
 }
 
 function List-VMs
@@ -45,6 +53,9 @@ do
     $input = Read-Host "what do you want to do?"
     switch ($input)
     {
+       'X' {               
+                Connect-NTNXcluster
+            }
         '1' {               
                 List-Vms
             }
