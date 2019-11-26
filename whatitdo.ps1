202c38b7b7f4e651adeadcce3a057c9eb41df883
cloudcor-ntnx <55808532+cloudcor-ntnx@users.noreply.github.com>
2019-11-26 13:32:32 -0800

    Update whatitdo.ps1

--- whatitdo.ps1
+++ whatitdo.ps1
@@ -60,3 +60,8 @@ do
     pause
 }
 until ($input -eq 'q')
+
+$myvarLoaded = Get-PSSnapin -Name NutanixCmdletsPSSnapin -ErrorAction SilentlyContinue | % {$_.Name}
+if ($myvarLoaded -eq $null){Add-PSSnapin NutanixCmdletsPSSnapin}
+$server = read-host "Your NTNX CLUSTER IP"
+Connect-NTNXCluster -Server $server -AcceptInvalidSSLCerts -ForcedConnection
