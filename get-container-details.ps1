9d0cdce054b0d02bbeb531fbbfeec2b20c434820
cloudcor-ntnx <55808532+cloudcor-ntnx@users.noreply.github.com>
2020-01-21 19:03:39 -0800

    Update get-container-details.ps1

--- get-container-details.ps1
+++ get-container-details.ps1
@@ -1,2 +1,10 @@
+	   <# 
+	   
+        .SYNOPSIS
+            list containers with select parameters
+        .EXAMPLE
+            $cluster = get-ntnxcluster |select ID, name, clusterExternalIPAddress, ClusterExternalDataServicesIPAddress, numNodes, blockSerials, version, externalsubnet, internalsubnet, nccversion, globalNfsWhiteList, hypervisorTypes, faultToleranceDomainType|format-list
+            $cluster
+    #>
 $container = Get-NTNXContainer | select @{name="Container_ID";expression={$_.id.substring($_.id.indexof(":") + 2)}},Name, markedForRemoval, replicationFactor, oplogReplicationFactor, nfsWhitelist |Sort-Object name |format-table -autosize 
 $container
