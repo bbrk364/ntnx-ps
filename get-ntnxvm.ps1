f782f0791f472b75fc2830d77d5b26b25435fcb2
cloudcor-ntnx <55808532+cloudcor-ntnx@users.noreply.github.com>
2020-01-21 18:59:08 -0800

    Update get-ntnxvm.ps1

--- get-ntnxvm.ps1
+++ get-ntnxvm.ps1
@@ -1,2 +1,12 @@
+	   <# 
+	   
+        .SYNOPSIS
+            get-vm with set parameters
+
+        .EXAMPLE
+            $cluster = get-ntnxcluster |select ID, name, clusterExternalIPAddress, ClusterExternalDataServicesIPAddress, numNodes, blockSerials, version, externalsubnet, internalsubnet, nccversion, globalNfsWhiteList, hypervisorTypes, faultToleranceDomainType|format-list
+            $cluster
+    #>
+
 $vms = get-ntnxvm |select ipaddresses, vmName, uuid, hostName, nonNdfsDetails, powerState |sort-object nonndfsdetails|ft -autosize
 $vms
