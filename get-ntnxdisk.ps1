9802f837b60c58f3181d3bf31ee98983532a5b6e
cloudcor-ntnx <55808532+cloudcor-ntnx@users.noreply.github.com>
2020-01-21 19:01:11 -0800

    Update get-ntnxdisk.ps1

--- get-ntnxdisk.ps1
+++ get-ntnxdisk.ps1
@@ -1,3 +1,12 @@
+	   <# 
+	   
+        .SYNOPSIS
+            get-disk information with the selected parameters
+        .EXAMPLE
+            $cluster = get-ntnxcluster |select ID, name, clusterExternalIPAddress, ClusterExternalDataServicesIPAddress, numNodes, blockSerials, version, externalsubnet, internalsubnet, nccversion, globalNfsWhiteList, hypervisorTypes, faultToleranceDomainType|format-list
+            $cluster
+    #>
+
 $clusterdisks = Get-NTNXDisk |select location, id, storagetiername, hostName, cvmipaddress, mountpath, markedforremoval, datamigrated, online,
 diskstatus, selfencryptingdrive |sort-object nonndfsdetails|format-list
 $clusterdisks
