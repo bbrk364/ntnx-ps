694dd007ebb209ef425976e3bac3feb4c1cb91f2
cloudcor-ntnx <55808532+cloudcor-ntnx@users.noreply.github.com>
2020-01-21 19:01:47 -0800

    Update get-ntnxcluster.ps1

--- get-ntnxcluster.ps1
+++ get-ntnxcluster.ps1
@@ -1,2 +1,11 @@
+	   <# 
+	   
+        .SYNOPSIS
+            get-cluster details with the selected parameters
+        .EXAMPLE
+            $cluster = get-ntnxcluster |select ID, name, clusterExternalIPAddress, ClusterExternalDataServicesIPAddress, numNodes, blockSerials, version, externalsubnet, internalsubnet, nccversion, globalNfsWhiteList, hypervisorTypes, faultToleranceDomainType|format-list
+            $cluster
+    #>
+
 $cluster = get-ntnxcluster |select ID, name, clusterExternalIPAddress, ClusterExternalDataServicesIPAddress, numNodes, blockSerials, version, externalsubnet, internalsubnet, nccversion, globalNfsWhiteList, hypervisorTypes, faultToleranceDomainType|format-list
 $cluster
