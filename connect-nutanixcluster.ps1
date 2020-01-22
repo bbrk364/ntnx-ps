6d80b9a0fea7498eeb175094583d7066b3212183
cloudcor-ntnx <55808532+cloudcor-ntnx@users.noreply.github.com>
2020-01-21 19:05:13 -0800

    Update connect-nutanixcluster.ps1

--- connect-nutanixcluster.ps1
+++ connect-nutanixcluster.ps1
@@ -1,3 +1,10 @@
+	   <# 
+	   
+        .SYNOPSIS
+            Loads Nutanix cmdlets snapin, prompts for cluster IP, connects to the cluster with invalid certs and forced connection
+    #>
+
+
 $myvarLoaded = Get-PSSnapin -Name NutanixCmdletsPSSnapin -ErrorAction SilentlyContinue | % {$_.Name}
 if ($myvarLoaded -eq $null){Add-PSSnapin NutanixCmdletsPSSnapin}
 $server = read-host "Your NTNX CLUSTER IP"
